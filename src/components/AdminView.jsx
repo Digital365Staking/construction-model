ed49a40153635f3c365cd2bbe3f81e5a3b61d7c8
Adrien FALCONNET <nuriaevento@gmail.com>
2025-03-04 22:46:51 +0100

    List last comments tested

--- src/components/AdminView.jsx
+++ src/components/AdminView.jsx
@@ -1,19 +1,53 @@
 import React, { useState, useEffect, useRef } from 'react';
 import "../styles/AdminView.css";
+import { GraphQLClient } from 'graphql-request';
 
 
+const client = new GraphQLClient(import.meta.env.VITE_GRAPHQL_URL, {
+    headers: {
+      "x-hasura-admin-secret": import.meta.env.VITE_GRAPHQL_KEY,
+    },
+  });
 
 const AdminView = () => {
     
     const lstMsgRef = useRef(null);
     const [messages, setMessages] = useState([]);
+    const [comments, setComments] = useState([]);
     
     const [selLang, setSelLang] = useState(import.meta.env.VITE_LANG);
     const curAI = selLang === 'es' ? 'Asistente virtual' : (selLang === 'en' ? 'Virtual assistant' : 'Assistant virtuel');
     const curMe = selLang === 'es' ? 'Yo' : (selLang === 'en' ? 'Me' : 'Moi');
 
-    useEffect(() => {
+    const curFormat = selLang === 'es' ? 'es-ES' : (selLang === 'en' ? 'en-US' : 'fr-FR');
+
+    const QUERY_COMMENTS = `
+    query {
+    COMMENT(
+      where: { id_client: { _eq: 1 } }
+      order_by: [{ pseudo: asc }, { created: desc }]
+      distinct_on: pseudo
+          ) {
+              pseudo
+              question
+              created
+          }
+      }
+      `;
+  
+        const fetchComments = async () => {
+            try {
+            const data = await client.request(QUERY_COMMENTS);
+            const sortedComments = data.COMMENT.sort((a, b) => new Date(b.created) - new Date(a.created));
+            setComments(sortedComments); 
+            } catch (error) {
+            console.error("Error fetching data:", error);
+            }      
+        };
+
+    useEffect(() => { 
         setMessages([]);
+        fetchComments();
         let fruits = [
             "Apple", "Banana", "Cherry", "Mango", "Pineapple", 
             "Strawberry", "Blueberry", "Grapes", "Orange", "Pear", 
@@ -21,7 +55,7 @@ const AdminView = () => {
             "Pomegranate", "Lemon", "Lime", "Fig", import.meta.env.VITE_LIMIT_LIST_ADMIN
         ];
         for (let fruit of fruits) {
-            loadMessage(curAI,fruit,"");
+            //loadMessage(curAI,fruit,"");
         }
         
     }, []);
@@ -52,12 +86,18 @@ const AdminView = () => {
       <div className="chat-container">       
         
         <div ref={lstMsgRef} className="chat-messages">           
-          {messages.map((message, index) => (            
+          {comments.map((item, index) => (            
             <div
               key={index}
-              className={`message ${message.sender === curMe ? 'blue-bg' : 'gray-bg'}`}>
-              <div className="message-sender">✉️​{message.text}
-              <div className="message-timestamp">{message.timestamp}</div>
+              className='message blue-bg'>
+              <div className="message-sender">✉️​{item.pseudo}
+              <div className="message-timestamp">{new Date(item.created).toLocaleString(curFormat, {
+                    month: 'long',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    hour24: true,
+                    })}</div>
               </div> 
                
 
