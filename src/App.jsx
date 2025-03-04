d1c5db61de6c044275a3067eb53efd267bdc0ded
Adrien FALCONNET <nuriaevento@gmail.com>
2025-03-04 12:30:27 +0100

    admin messages template

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 // App.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import ClientView from './components/ClientView';
 import AdminView from './components/AdminView';
 
 
 const App = () => {
+  const [curGuid, setCurGuid] = useState(() => JSON.parse(localStorage.getItem('curGuid')) || "");
+
+  useEffect(() => {
+          localStorage.setItem('curGuid', JSON.stringify(curGuid));
+          
+      }, [curGuid]);
+
   return (
     <div>      
       <main>  
-        <AdminView />        
+        {curGuid !== "" && (<AdminView />)} 
+        {curGuid === "" && (<AdminView />)}       
       </main>      
     </div>
   );
